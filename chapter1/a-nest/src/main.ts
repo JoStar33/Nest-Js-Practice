385f87b4ee7befa9ddea8f9ca43bbeb1fad268d2
HoCG <rhwkd5012@naver.com>
2024-09-16 21:03:21 +0900

    feat: hot reloading 설정

--- chapter1/a-nest/src/main.ts
+++ chapter1/a-nest/src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 // 여기가 Nest실행 장소이다.
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
   const port = process.env.PORT || 3000;
   console.log(`listening on port ${port}`); // 3000!
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
