86a429e0c7235ff09ec77ed4e165bdb5c66987b2
HoCG <rhwkd5012@naver.com>
2024-09-17 00:29:12 +0900

    feat: env정보 설정방법

--- chapter1/a-nest/src/app.module.ts
+++ chapter1/a-nest/src/app.module.ts
@@ -1,15 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 // Nest는 모듈시스템으로 구성돼 있음.
 // Express는 라우터를 추가해주는 방식으로 설계를 했다면, Nest는 모듈위주의
 // 설계를 하게된다. Nest를 실행할때 나오는 메시지를 통해 연결을 확인가능하다.
 // 다만 모듈을 직접 구성해야한다는 점에서는 스프링보다 IoC가 약하다.
 
+const getEnv = async () => {
+  // 이렇게 비밀키정보를 API호출을 통해 가져오는것 또한 가능하다.
+  const response = await fetch('/key');
+  return response.body;
+};
+
 @Module({
-  imports: [],
+  // forRoot가 뭐냐면, 가끔가다 register나 forFeature 이런식으로 선언하는데,
+  // 매개변수를 받아야해서 이렇게쓰는 경우가 있음.
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [getEnv] })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
--- chapter1/a-nest/src/app.service.ts
+++ chapter1/a-nest/src/app.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 // 서비스의 역할은 비즈니스 로직의 분리이다.
 // 이친구는 요청과 응답에 대해서는 모른다.
 // 서비스로 분리해두면, 중복된 기능을 사용하는 상황에서 유용하다.
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
   // req, res이런건 안넣는게 좋고, 해야하는 동작만 넣어주는게 좋다.
   getHello(): string {
+    // 참고로 .env파일 만들때 .env.prod, .env.dev 이렇게 만들어서 쓰는것도 가능함.
+    // 환경변수별로 나뉘어서 쓰는것도 가능함.
+    // 환경변수도 사실 Nest랑 관련이 없는거잖아? 이런 통제권까지도 Nest에게 넘기는거임ㅇㅇ
+    console.log(process.env.TEST);
+
+    // 아래와 같이 configService를 통해 사용도 가능함.
+    // 이러면 TEST 정보를 가져올 수 있음.
+    // 걍 아래처럼쓰자!!!!
+    this.configService.get('TEST');
     return 'Hello World!';
   }
   // 그리고 좋은점! 이렇게 결과데이터 그자체로 리턴만 하면 알아서 JSON으로 출력된다.
