d7cef209d56b94d70592f7a1757fddc0b16aac24
JoStar33 <rhwkd5012@naver.com>
2024-09-17 16:27:20 +0900

    docs: 간단한 설명 작성

--- chapter1/a-nest/src/main.ts
+++ chapter1/a-nest/src/main.ts
@@ -6,6 +6,8 @@ declare const module: any;
 // 여기가 Nest실행 장소이다.
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app에 use를 통해 뭘 적용하는것도 Express랑 비슷함.
+  // passport나 쿠키파서같은거 연결하는건 app.use(cookieParser())이렇게 하면 됨.
   await app.listen(3000);
   const port = process.env.PORT || 3000;
   console.log(`listening on port ${port}`); // 3000!
